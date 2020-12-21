3c8d7674764aaef85322102039d021e6cd4e8696
Alex Santos <alexdsantosv@gmail.com>
2020-12-21 20:19:19 +0100

    prueba

--- menu.py
+++ menu.py
@@ -3,16 +3,9 @@ import os
 
 
 def menu(options, actions):
-    # LIMPIAR PANTALLA
     os.system('clear' if os.name == 'posix' else 'cls')
-
-    # MOSTRAR OPCIONES
     hr = "-" * (len(options) + 4)
     print(f'{hr}\n| {options} |\n{hr}')
-
-    # ASIGNAR ACCION
     option = input('Escribe una opción: ')
-
-    # LISTA DE OPCIONES
     func = actions.get(int(option), lambda: menu(options, actions))
     func()
